faca7e0a9fd339478ffc64ff5a365bba8f02292e
OG-1534 <gershomochanda@gmail.com>
2024-07-04 23:43:25 +0300

    Given the parameters and the return values, add type annotations to the function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -16,6 +16,7 @@ import typing
 
 T = typing.TypeVar('T')
 
+
 def safely_get_value(dct: typing.Mapping, key: typing.Any, default:
                      typing.Union[T, None] = None) -> \
         typing.Union[typing.Any, T]:
